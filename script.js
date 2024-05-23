1f90a4f4d6d41c7554c0a402b4008c7ef208bba5
Téofane <111658799+teofane@users.noreply.github.com>
2024-05-23 22:21:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,54 @@
 let nbCookies = 0;
 let cps = 0;
-let cpc = 10;
+let cpc = 1;
 
 function clique(){
-    nbCookies = nbCookies + cpc;
+    nbCookies = Math.round((nbCookies + cpc)*10) / 10;
     document.getElementById("nombre").innerText = nbCookies;
-    return nbCookies
+    return nbCookies;
 }
 
 function parSeconde(){
-    nbCookies = nbCookies + cps;
+    nbCookies = Math.round((nbCookies + cps)*10) / 10;
     document.getElementById("nombre").innerText = nbCookies;
-    document.getElementById("cps").innerText = "par seconde : "+cps;
+    document.getElementById("cpstot").innerText = "par seconde : "+ Math.round((cps)*10) / 10;
 }
 
 setInterval(parSeconde, 1000);
 
 // Batiments
 
-let cursorPrix = 150;
-let cursorCps = 1;
+let cursorPrix = 15;
+let cursorCps = 0.1;
 let cursorNombre = 0;
 function cursor(){
-    if (nbCookies-cursorPrix > 0){
-        nbCookies = nbCookies - cursorPrix;
+    if (nbCookies-cursorPrix >= 0){
+        nbCookies = Math.round((nbCookies-cursorPrix)*10) / 10;
         cursorNombre++;
-        cursorPrix = (cursorPrix * (1.15**cursorNombre)).toFixed();
+        cursorPrix = (15 * (1.15**cursorNombre)).toFixed();
         cps = cps + cursorCps;
-        document.getElementById("cursor").innerText = "Cps : "+cursorCps+", Prix : "+cursorPrix+", Nombre :"+cursorNombre;
+        document.getElementById("cursorCps").innerText = "Cps : " + cursorCps;
+        document.getElementById("cursorPrix").innerText = "Prix : " + cursorPrix;
+        document.getElementById("cursorNombre").innerText = "Nombre : " + cursorNombre;
         document.getElementById("nombre").innerText = nbCookies;
     }
 }
 
-let canardPrix = 1000;
-let canardCps = 10;
+let canardPrix = 100;
+let canardCps = 1;
 let canardNombre = 0;
 function canard(){
-    if (nbCookies-canardPrix > 0){
+    if (nbCookies-canardPrix >= 0){
         canardNombre ++;
         nbCookies = nbCookies - canardPrix;
-        canardPrix = (canardPrix * (1.15**canardNombre)).toFixed();
-        cps = cps + canardCps
-        document.getElementById("Canard").innerText = "Cps : "+canardCps+", Prix : "+canardPrix+", Nombre :"+canardNombre;
+        canardPrix = (100 * (1.15**canardNombre)).toFixed();
+        cps = cps + canardCps;
+        document.getElementById("canardCps").innerText = "Cps : " + canardCps;
+        document.getElementById("canardPrix").innerText = "Prix : " + canardPrix;
+        document.getElementById("canardNombre").innerText = "Nombre : " + canardNombre;
         document.getElementById("nombre").innerText = nbCookies;
+    }
+    else{
+        alert("Bah non t'es trop pauvre sale gueux")
     }
 }
